b24499adae58f29ca6aa347a9f5299e8bbad9a0b
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-05-04 01:34:53 -0400

    worked on nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Banner from "@/components/Banner";
 import PopularCourses from "@/components/PopularCourses";
-import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
-
-      <PopularCourses></PopularCourses>
+      <Suspense
+        fallback={<h1 className="text-xl text-center">Data is loading...</h1>}
+      >
+        <PopularCourses></PopularCourses>
+      </Suspense>
     </div>
   );
 }
