ec216f5e2d2aca1640e6ea80101ccfdbd052a15d
arshadhossainoh <arshadhossain.oh@gmail.com>
2026-05-04 01:07:54 -0400

    pcourses

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,16 @@
 import Banner from "@/components/Banner";
+import PopularCourses from "@/components/PopularCourses";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <Banner></Banner>;
+  return;
+  <div>
+    <Banner></Banner>
+    <Suspense
+      fallback={<h1 className="text-xl text-center">Data is loading...</h1>}
+    >
+      <PopularCourses></PopularCourses>
+    </Suspense>
+  </div>;
 }
